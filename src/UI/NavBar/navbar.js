be3206f7ee6cc0a01c369a573b4b23470a8d0db3
marlorodrigues <49593773+marlorodrigues@users.noreply.github.com>
2020-05-25 23:47:19 -0300

    Update navbar.js

--- src/UI/NavBar/navbar.js
+++ src/UI/NavBar/navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-dark">
             <a className="navbar-brand" href="-">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@ const NavBar = () => {
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <button className="btn btn-outline-primary" type="submit">Search</button>
                 </form>
             </div>
         </nav>
